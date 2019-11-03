3c36035769a8a7a3e8f7f41d123a6b999d517679
hodginson <rawsonzane@gmail.com>
2019-11-03 20:28:11 +1300

    should work on heroku now

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var checkoutRouter = require('./routes/checkout');
 
 
 var urlencodedParser= bodyParser.urlencoded({extended: false});
-const {
+/*const {
   Pool
 } = require('pg');
 const pool = new Pool({
@@ -29,14 +29,14 @@ const pool = new Pool({
   ssl: true,
 });
 
-pool.connect();
+pool.connect();*/
 
 
-/*const { Pool } = require('pg');
+const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
-});*/
+});
 
 
 
