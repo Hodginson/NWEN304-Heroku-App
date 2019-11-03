144a4379ee8132f80480b7f215ea0fc4448ed81b
hodginson <rawsonzane@gmail.com>
2019-11-03 16:57:41 +1300

    should work on heroku now

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ var checkoutRouter = require('./routes/checkout');
 
 
 var urlencodedParser= bodyParser.urlencoded({extended: false});
-const { Pool } = require('pg');
+/*const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
-});
+});*/
 
 
 var oktaClient = new okta.Client({
