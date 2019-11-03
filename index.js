249cf199e94aea4614a78122c04c01b11da80081
hodginson <rawsonzane@gmail.com>
2019-11-03 14:23:19 +1300

    should work on heroku now

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/',function(req,res){
 });
 
 app.get('/browse', (req, res) => res.render('browse'));
-app.get('/login', (req, res) => res.render('login'));
+app.get('/login', (req, res) => res.render('Login'));
 
 // Get all tasks.
 app.get('/book', function (req, res) {
