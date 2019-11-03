a3caba1f6493743ab390bc804375f11d69cfb1e9
hodginson <rawsonzane@gmail.com>
2019-11-03 16:19:44 +1300

    should work on heroku now

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
-//app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
