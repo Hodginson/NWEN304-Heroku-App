a1143e8ac70840a25e147da2e55037cc121192cc
Huaiqu Cai <caihuai@traffick.ecs.vuw.ac.nz>
2019-11-03 18:40:26 +1300

    signuo6

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,33 +1,31 @@
 $(document).ready(function(e) {
 
-  $('#signUp').button().click(signUp());
+    $('#signUp').button().click(
+		function() {
+			var userEmail = $('#email').val();
+            var password = $('#pass').val();
+            var rePassword = $('#repass').val();
+            console.log(userEmail);
+            if(userEmail === '' || password ===''||rePassword===''){
+               alert("Please Confirm your input");
+            }
+            if(password !== rePassword){
+               alert("Please Confirm your Password");
+            }
 
-  $('#cancel').button().click(
-  function() {
-     window.location = "home.html";
-  });
+            queryAPI('POST', '/signUp', {
+              username:userEmail,
+              password: password
+            }, function(){});
+
+		});
+    $('#cancel').button().click(
+   function() {
+       window.location = "home.html";
+    });
+
 
 }); // end ready
-
-
-function signUp() {
-  var userEmail = $('#email').val();
-        var password = $('#pass').val();
-        var rePassword = $('#repass').val();
-        console.log(userEmail);
-        if(userEmail === '' || password ===''||rePassword===''){
-           alert("Please Confirm your input");
-        }
-        if(password !== rePassword){
-           alert("Please Confirm your Password");
-        }
-
-        queryAPI('POST', '/signUp', {
-          username:userEmail,
-          password: password
-        }, function(){});
-
-}
 
 
 function queryAPI(method, path, data, callback) {
@@ -35,7 +33,7 @@ function queryAPI(method, path, data, callback) {
   $.ajax({
     method: method,
     url: 'https://nwne304-group-17.herokuapp.com' + path,
-    data: data,
+    data: JSON.stringify(data),
     contentType: "application/json",
     dataType: "json",
     success: function (res) {
