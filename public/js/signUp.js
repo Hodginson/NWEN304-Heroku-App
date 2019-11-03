32015358fea62d176681e6b7b38053133abd7690
Huaiqu Cai <caihuai@traffick.ecs.vuw.ac.nz>
2019-11-03 17:57:59 +1300

    siguPJS

---
+++ public/js/signUp.js
@@ -0,0 +1,23 @@
+$(document).ready(function(e) {
+
+    $('#signUp').button().click(
+		function() {
+			var userEmail = $('#email').val();
+            var password = $('#pass').val();
+            var rePassword = $('#repass').val();
+            console.log(userEmail);
+            if(userEmail === '' || password ===''||rePassword===''){
+               alert("Please Confirm your input");
+            }
+            if(password !== rePassword){
+               alert("Please Confirm your Password");
+            }
+            $.post("/register",{"username":userEmail,"password":password}, function (data, status) {})
+		});
+    $('#cancel').button.click(
+   function() {
+       window.location = "home.html";
+    });
+
+
+}); // end ready
