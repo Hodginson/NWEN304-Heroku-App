2fe1bc234a9e1dc7adf35734f131a3c301122df1
hodginson <rawsonzane@gmail.com>
2019-11-04 16:59:47 +1300

    add

--- app.js
+++ app.js
@@ -288,11 +288,7 @@ app.post('/signUp', function (req,res){
 //isLoggedIn
 app.get('/isSignedIn', async (req, res) => {
   try {
-          res.send(req.session.user.id);
-
-
-
-
+          res.send(req.user.id);
       console.log();
       // Need a way to send the users email through here? Hmm
       //res.send(req.user.username); // someone currently logged in
@@ -339,70 +335,6 @@ app.get('/isSignedIn', async (req, res) => {
    });
  });
 
- //*******SEAN DB******GET REQUEST (item_table)*********************/
- app.get('/api/items', async (req, res) => {
-  try {
-    const client = await pool.connect()
-    var result = await client.query('SELECT * FROM item_table;');
-
-    if (!result) {
-      return res.send('No data found');
-      }else{
-      result.rows.forEach(row=>{
-      console.log(row);
-      });
-      }
-
-  res.send(result.rows);
-  client.release();
-
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
-
-
-
- //*******SEAN DB****** Delete REQUEST (cart_table)*********************/
-app.delete('/api/items',urlencodedParser, async (req,res)=>{
-  try {
-    const client = await pool.connect();
-    var result = await client.query("DELETE FROM cart_table WHERE cart_id="+999+" AND item_id="+req.body.item_id+";");
-
-
-    if (!result) {
-         return res.send("DELETION Failure");
-       } else {
-         console.log("successful");
-       }
-       res.send(result.rows);
-       client.release();
-     } catch (err) {
-       console.error(err);
-       res.send("Error " + err);
-  }
- });
-
-
-  //*******Andy DB for Zihui's clear database****** Delete REQUEST (cart_table)*********************/
-app.delete('/api/clear',urlencodedParser, async (req,res)=>{
-  try {
-    const client = await pool.connect();
-    var result = await client.query('delete from cart_table;');
-    if (!result) {
-         return res.send("DELETION Failure");
-       } else {
-         console.log("successful");
-       }
-       res.send(result.rows);
-       client.release();
-     } catch (err) {
-       console.error(err);
-       res.send("Error " + err);
-  }
- });
-
 
 
 //******Zane***** PUT Request*************/
@@ -444,38 +376,6 @@ app.put('/buyBook', function(req,res){
   });
 
 });
-
-
-
-
-
-//******Andy DB*****POST Request*************/
-app.post('/api/products',urlencodedParser, async (req,res)=>{
-  try {
-    const client = await pool.connect();
-
-    var r= await client.query('select item_id from  cart_table where item_id='+req.body.item_id+';');
-    if((r.rows).length==0){
-      var result = await client.query('insert into cart_table values ('+req.body.cart_id+', '+req.body.item_id+', '+req.body.item_quantity+');' );
-    }else{  //if there are rows with same item_id
-      var old_value= await client.query('select item_quantity from cart_table where item_id='+req.body.item_id+';');
-      res.send(old_value);
-    }
-    if (!result) {
-         return res.send("POST Failure");
-       } else {
-         console.log("successful");
-       }
-       res.send(result.rows);
-       client.release();
-     } catch (err) {
-       console.error(err);
-       res.send("Error " + err);
-  }
- });
-
-
-
 
 
 // catch 404 and forward to error handler
