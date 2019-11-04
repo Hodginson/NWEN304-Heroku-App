e546fcc37395dd0413d7430f2a2182eabb0e7878
hodginson <rawsonzane@gmail.com>
2019-11-04 15:14:08 +1300

    setting up the login

--- models/user.js
+++ models/user.js
@@ -44,7 +44,7 @@ var User = sequelize.define('users', {
         user.password = bcrypt.hashSync(user.password, salt);
       }
     }
-  }
+
 
       User.prototype.validPassword= function(password) {
         return bcrypt.compareSync(password, this.password);
