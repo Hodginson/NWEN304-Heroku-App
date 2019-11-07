e3e3baf109c0bfd73406735932ad77126db4bc11
hodginson <rawsonzane@gmail.com>
2019-11-08 11:36:18 +1300

    'search'

--- public/js/CategoryButtons.js
+++ public/js/CategoryButtons.js
@@ -1,6 +1,15 @@
-
-
 function loadFiction() {
-
   window.location.href = "store.html?=" + "Fiction";
+}
+
+function loadCrime() {
+  window.location.href = "store.html?=" + "Crime";
+}
+
+function loadAutoBiography() {
+  window.location.href = "store.html?=" + "AutoBiography";
+}
+
+function loadChildrens() {
+  window.location.href = "store.html?=" + "Childrens Book";
 }
