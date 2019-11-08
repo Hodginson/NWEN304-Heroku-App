40194be496d26ae9c8e0bd0b820fee139c9618f9
hodginson <rawsonzane@gmail.com>
2019-11-08 17:16:43 +1300

    'search'

--- public/js/cart.js
+++ public/js/cart.js
@@ -29,14 +29,13 @@ $(document).ready(function (e) {
 })
 
 function setupFunction(books) {
-  var x;
-  var split;
+  var x = cart.toString();
+  var split = x.split(',');
   if(split.length==0){
     alert("you have nothing in your cart");
     window.location.href = "store.html";
   }
-  x = cart.toString();
-  split = x.split(',');
+
   console.log(split);
   if(split[0] == ""){
     alert("you have nothing in your cart");
