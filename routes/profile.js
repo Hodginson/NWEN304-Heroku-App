eaefb884a335abb1c82e2c42774103662022576a
hodginson <rawsonzane@gmail.com>
2019-11-08 18:59:53 +1300

    'search'

--- routes/profile.js
+++ routes/profile.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+var path = require('path');
+
 // Home page
 router.get("/", (req, res) => {
-  res.sendFile("UserPage.html");
+  res.sendFile(path.resolve(__dirname, '../public/UserPage.html'));
 });
 
 module.exports = router;
